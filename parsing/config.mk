16202988e8cdb0dc16bfb10d3698435842b18ab7
Oliver Katz <code.sn1@gmail.com>
2013-07-16 08:33:10 -0400

    made mc independant and changed the main makefile to be more useful.

--- parsing/config.mk
+++ parsing/config.mk
@@ -2,11 +2,11 @@ SYSTEM=$(shell uname -s)
 
 ifeq ($(SYSTEM),Darwin)
 CXX=c++
-CXXFLAGS=-std=c++11 -stdlib=libc++ -ggdb -O0
+CXXFLAGS=-std=c++11 -stdlib=libc++ -ggdb -O0 -I/usr/include/mitten -L/usr/lib/mitten
 CXXSHAREDFLAGS=-dynamiclib
 else
 CXX=g++
-CXXFLAGS=-std=c++11 -ggdb -O0
+CXXFLAGS=-std=c++11 -ggdb -O0 -Imitten -Lmitten
 endif
 
 RM=rm
--- trace/config.mk
+++ trace/config.mk
@@ -1,29 +1,12 @@
-# This file is part of Mitten.
-# 
-# Mitten is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# Mitten is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with Mitten.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Copyright 2013 Oliver Katz
-
 SYSTEM=$(shell uname -s)
 
 ifeq ($(SYSTEM),Darwin)
 CXX=c++
-CXXFLAGS=-std=c++11 -stdlib=libc++ -ggdb -O0
+CXXFLAGS=-std=c++11 -stdlib=libc++ -ggdb -O0 -I/usr/include/mitten -L/usr/lib/mitten
 CXXSHAREDFLAGS=-dynamiclib
 else
 CXX=g++
-CXXFLAGS=-std=c++11 -ggdb -O0
+CXXFLAGS=-std=c++11 -ggdb -O0 -Imitten -Lmitten
 endif
 
 RM=rm
