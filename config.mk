d08e68df66bbd3b63f57d45c780287ca805b46f8
Oliver Katz <code.sn1@gmail.com>
2013-07-17 08:51:48 -0400

    fixed linux support.

--- config.mk
+++ config.mk
@@ -4,11 +4,12 @@ ifeq ($(SYSTEM),Darwin)
 CXX=c++
 CXXFLAGS=-std=c++11 -stdlib=libc++ -ggdb -O0 -I/usr/include/mitten
 CXXSHAREDFLAGS=-dynamiclib
-LDFLAGS=-L/usr/include/mitten
+LDFLAGS=-L/usr/lib/mitten
 else
 CXX=g++
-CXXFLAGS=-std=c++11 -ggdb -O0 -I/usr/include/mitten
-LDFLAGS=-L/usr/include/mitten
+CXXFLAGS=-std=c++11 -ggdb -O0 -I/usr/include/mitten -fPIC
+CXXSHAREDFLAGS=-shared
+LDFLAGS=-L/usr/lib/mitten
 endif
 
 RM=rm
--- mc/core/vm.h
+++ mc/core/vm.h
@@ -24,6 +24,7 @@
 #include <vector>
 #include <sstream>
 
+#ifdef __OSX__
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
@@ -35,6 +36,19 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/ExecutionEngine/JIT.h>
+#else
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/PassManager.h>
+#include <llvm/Analysis/Verifier.h>
+#include <llvm/Analysis/Passes.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/ExecutionEngine/JIT.h>
+#endif
 
 #define VM_WARN cout<<"[Mitten VM] "
 
